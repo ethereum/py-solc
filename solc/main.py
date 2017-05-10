e1db73ea4de0b4110aa5825f307f77f6f98b1bba
Alan Lu <alanlu1023@gmail.com>
2017-05-10 15:41:41 -0500

    Typo

--- solc/main.py
+++ solc/main.py
@@ -143,13 +143,13 @@ def compile_files(source_files,
     return contracts
 
 
-def link_code(unliked_data, libraries):
+def link_code(unlinked_data, libraries):
     libraries_arg = ','.join((
         ':'.join((lib_name, lib_address))
         for lib_name, lib_address in libraries.items()
     ))
     stdoutdata, stderrdata, _, _ = solc_wrapper(
-        stdin_bytes=unliked_data,
+        stdin_bytes=unlinked_data,
         link=True,
         libraries=libraries_arg,
     )
