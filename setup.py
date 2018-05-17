62296971103944be196cc9fa6b6988c7b8132fca
Piper Merriam <pipermerriam@gmail.com>
2018-05-17 11:56:08 -0600

    Bump version: 2.2.0 → 3.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import (
 setup(
     name='py-solc',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='2.2.0',
+    version='3.0.0',
     description="""Python wrapper around the solc binary""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
